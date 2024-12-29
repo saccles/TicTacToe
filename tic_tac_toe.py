1864aa0382609c0eff9a3235f9f320878e18ad21
saccles <silasaccles@gmail.com>
2024-12-28 21:11:08 -0500

    Reformat tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,6 @@
 # Author: Silas Accles
 # Created: June 6, 2024
-# Last Modified: October 3, 2024
+# Last Modified: December 28, 2024
 
 from random import choice
 from time import sleep
@@ -8,29 +8,9 @@ from time import sleep
 # Initialize the one-dimensional array that represents the tic tac toe board.
 game_board = [" " for field in range(9)]
 
-instructions = """
-|---------------------------------------------------------------------|
-|How To Play:                                                         |                                                                   |
-| - When the game starts, the tic tac toe board is displayed.         |
-| - The computer then makes a move.                                   |
-| - Once the computer has made its move, you may make your move.      |
-| - When that time comes along, choose a number from 1 to 9.          |
-| - This number corresponds to the fields on the tic tac toe board,   |
-|   which are numbered left to right, top to bottom.                  |
-| - To win, get three o's in a row on the tic tac toe board           |
-|                                                                     |  
-|Example:                                                             |                                                                |
-|If a player wanted to choose the field in the upper right-hand       |
-|corner of the tic tac toe board, they would choose the number 3.     |
-|                                                                     |
-|Good luck!                                                           |
-|---------------------------------------------------------------------|
-"""
-closing_message = "Thanks for playing!"
-
 
 def display_game_board():
-    """A function that displays the tic tac toe board."""
+    """Display an ASCII representation of the tic tac toe board."""
 
     # ASCII representation of the tic tac toe board at any time.
     print(
@@ -45,26 +25,35 @@ def display_game_board():
 
 
 def get_computer_move():
-    """A function that randomly chooses a field for the computer to occupy."""
+    """
+    Randomly choose and return a field on the tic tac toe board
+    for the computer to occupy.
+    """
     computer_move = choice(range(9))
 
     return computer_move
 
 
 def get_user_move():
-    """A function that prompts the user to choose a field to occupy."""
+    """
+    Prompt the user to choose a field on the tic tac toe board
+    to occupy and return the field.
+    """
     user_move = int(input("Choose a number from 1 to 9: "))
 
     return (user_move - 1)
 
 
 def validate_computer_move(computer_move):
-    """A function that validates the computer's move."""
+    """
+    Return True if the computer's move is valid or 
+    False if the computer's move is invalid.
+    """
 
     # If the field on the tic tac toe board is empty,
-    # return true.
+    # return True.
     # Otherwise, if the field on the tic tac toe board
-    # has already been filled, return false.
+    # has already been filled, return False.
     if game_board[computer_move] == " ":
         return True
     elif (game_board[computer_move] == "x") or (
@@ -74,14 +63,17 @@ def validate_computer_move(computer_move):
 
 
 def validate_user_move(user_move):
-    """A function that validates the user's move."""
+    """
+    Return True if the user's move is valid or 
+    False if the user's move is invalid.
+    """
 
     # If the user's move is in the proper range and the tic tac toe field
-    # is empty, return true.
+    # is empty, return True.
     # Otherwise, if the user's move is not in the proper range,
-    # return false.
+    # return False.
     # Otherwise, if the user's move is in the proper range and the tic tac
-    # toe field has already been filled, return false.
+    # toe field has already been filled, return False.
     if (user_move in range(9)) and (game_board[user_move] == " "):
         return True
     elif user_move not in range(9):
@@ -93,7 +85,7 @@ def validate_user_move(user_move):
 
 
 def update_game_board_computer(computer_move):
-    """A function that updates the game_board with the computer's move."""
+    """Update the game board with the computer's move."""
 
     # Keep getting a new move until the move is valid.
     while validate_computer_move(computer_move) == False:
@@ -104,7 +96,7 @@ def update_game_board_computer(computer_move):
 
 
 def update_game_board_user(user_move):
-    """A function that updates the game_board with the user's move."""
+    """Update the game board with the user's move."""
 
     # Keep prompting the user for a new move until the move is valid.
     while validate_user_move(user_move) == False:
@@ -115,7 +107,7 @@ def update_game_board_user(user_move):
 
 
 def get_game_status():
-    """A function that gets the status of the tic tac toe game at any time."""
+    """Return the status of the tic tac toe game at any time."""
     game_status = ""
 
     # If the computer has three x's in a row or other combination,
@@ -168,18 +160,35 @@ def get_game_status():
     return game_status
 
 
-print("--------------------")
-print("Tic ... Tac ... Toe!")
-print("--------------------")
-print(f"\n{instructions}")
-print("Game board:")
-display_game_board()
-sleep(2)
-
-
 def main():
-    """A function that runs the program."""
-
+    INSTRUCTIONS = """
+    |---------------------------------------------------------------------|
+    |How To Play:                                                         |                                                                   |
+    | - When the game starts, the tic tac toe board is displayed.         |
+    | - The computer then makes a move.                                   |
+    | - Once the computer has made its move, you may make your move.      |
+    | - When that time comes along, choose a number from 1 to 9.          |
+    | - This number corresponds to the fields on the tic tac toe board,   |
+    |   which are numbered left to right, top to bottom.                  |
+    | - To win, get three o's in a row on the tic tac toe board           |
+    |                                                                     |  
+    |Example:                                                             |                                                                |
+    |If a player wanted to choose the field in the upper right-hand       |
+    |corner of the tic tac toe board, they would choose the number 3.     |
+    |                                                                     |
+    |Good luck!                                                           |
+    |---------------------------------------------------------------------|
+    """
+    CLOSING_MESSAGE = "Thanks for playing!"
+    
+    print("--------------------")
+    print("Tic ... Tac ... Toe!")
+    print("--------------------")
+    print(f"\n{INSTRUCTIONS}")
+    print("Game board:")
+    display_game_board()
+    sleep(2)
+    
     # Initiate the main program loop.
     while True:
         computer_move = get_computer_move()
@@ -192,15 +201,15 @@ def main():
 
         if game_status == "computer victory":
             print("I win!")
-            print(closing_message)
+            print(CLOSING_MESSAGE)
             break
         elif game_status == "user victory":
             print("You win!")
-            print(closing_message)
+            print(CLOSING_MESSAGE)
             break
         elif game_status == "no victory":
             print("The game ended, but neither of us won.")
-            print(closing_message)
+            print(CLOSING_MESSAGE)
             break
 
         user_move = get_user_move()
@@ -213,18 +222,17 @@ def main():
 
         if game_status == "computer victory":
             print("I win!")
-            print(closing_message)
+            print(CLOSING_MESSAGE)
             break
         elif game_status == "user victory":
             print("You win!")
-            print(closing_message)
+            print(CLOSING_MESSAGE)
             break
         elif game_status == "no victory":
             print("The game ended, but neither of us won.")
-            print(closing_message)
+            print(CLOSING_MESSAGE)
             break
 
 
-# If the statement evaluates to true, start the tic tac toe game.
 if __name__ == "__main__":
     main()
